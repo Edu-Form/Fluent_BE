7894e674fb6d9b0a84164beb0659c20be8a077c5
Phil Jin Sang <phil@Philui-MacBookPro.local>
2024-12-05 01:24:06 +0900

    modified delete and diary api

--- ai.py
+++ ai.py
@@ -2,13 +2,14 @@ import load_dotenv
 import os
 from openai import OpenAI
 
-# Load environment variables from the .env file
-load_dotenv.load_dotenv()
+# # Load environment variables from the .env file
+# load_dotenv.load_dotenv()
 
-# Access the OpenAI API key
-openai_api_key = os.getenv("OPENAI_API_KEY")
+# # Access the OpenAI API key
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# api_key = "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"
 
-client = OpenAI(api_key = "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")
+client = OpenAI()
 
 def ai_diary(original_text):
     completion = client.chat.completions.create(
